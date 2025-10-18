b84527365756bf7899c8935286b31714fe8df004
bd98z686sd-ui <bd98z686sd@privaterelay.appleid.com>
2025-10-18 20:19:21 +1100

    Delete src directory

--- src/useSnackbar.js
+++
@@ -1,6 +0,0 @@
-import {useCallback,useState} from 'react'
-export function useSnackbar(){
-  const[open,setOpen]=useState(false),[message,setMessage]=useState(''),[kind,setKind]=useState('ok')
-  const show=useCallback((m,k='ok',ttl=2400)=>{setMessage(m);setKind(k);setOpen(true); if(ttl)setTimeout(()=>setOpen(false),ttl)},[])
-  const close=()=>setOpen(false); return {open,message,kind,show,close}
-}
