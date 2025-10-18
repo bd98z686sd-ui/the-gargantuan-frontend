82bcec33f19cc7774154dda3c33d50addf71fc6d
bd98z686sd-ui <bd98z686sd@privaterelay.appleid.com>
2025-10-18 20:28:32 +1100

    Delete src directory

--- src/useAdminToken.js
+++
@@ -1,3 +0,0 @@
-import {useEffect,useState} from 'react'
-const KEY='gargantuan_admin_token'
-export function useAdminToken(){const[token,setToken]=useState('');useEffect(()=>{const s=localStorage.getItem(KEY);if(s)setToken(s)},[]);useEffect(()=>{localStorage.setItem(KEY,token||'')},[token]);return{token,setToken}}
