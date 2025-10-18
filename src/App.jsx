1499548e0a3bf787c86a5db798271d3d20a97ab4
bd98z686sd-ui <bd98z686sd@privaterelay.appleid.com>
2025-10-18 12:57:34 +1100

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center">
+      <h1 className="text-5xl mb-3 italic font-bold">The Gargantuan</h1>
+      <p className="text-lg">Music. Narration. Headlines. Daily.</p>
+    </div>
+  )
+}
