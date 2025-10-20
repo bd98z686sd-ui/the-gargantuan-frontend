9e51cbaef6764122438387470fc5740ee07b048d
bd98z686sd-ui <bd98z686sd@privaterelay.appleid.com>
2025-10-20 12:06:56 +1100

    Delete src directory

--- src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-guardianBlue text-white py-3 text-center text-sm fixed bottom-0 left-0 right-0 border-t-4 border-guardianRed">
-      © 2025 The Gargantuan · Contact: hellogargantuan69@gmail.com
-    </footer>
-  );
-}
--- src/router.js
+++
@@ -1,7 +0,0 @@
-export function useHashRoute() {
-  const get = () => (location.hash.replace(/^#\/?/, '') || '').split('?')[0] || ''
-  let subs = new Set()
-  const subscribe = (fn) => { subs.add(fn); return () => subs.delete(fn) }
-  window.addEventListener('hashchange', () => subs.forEach(fn => fn(get())))
-  return { get, subscribe }
-}
